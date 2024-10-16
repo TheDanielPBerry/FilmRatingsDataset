8b815179982028aa2431a7eda4e8f75a4f3a7164
Daniel Berry <dberry@sagecomputing.com>
2024-10-16 16:28:15 -0400

    Publish initial dataset

---
+++ sql/survey.sql
@@ -0,0 +1,30 @@
+INSERT INTO survey (id,email,ip_address) VALUES
+	 (1,'Survey #1',NULL),
+	 (2,'Survey #2',NULL),
+	 (3,'Survey #3',NULL),
+	 (5,'Survey #5',NULL),
+	 (6,'Survey #6',NULL),
+	 (7,'Survey #7',NULL),
+	 (8,'Survey #8',NULL),
+	 (9,'Survey #9',NULL),
+	 (10,'Survey #10',NULL),
+	 (11,'Survey #11',NULL);
+INSERT INTO survey (id,email,ip_address) VALUES
+	 (12,'Survey #12',NULL),
+	 (14,'Survey #14',NULL),
+	 (15,'Survey #15',NULL),
+	 (16,'Survey #16',NULL),
+	 (17,'Survey #17',NULL),
+	 (18,'Survey #18',NULL),
+	 (19,'Survey #19',NULL),
+	 (20,'Survey #20',NULL),
+	 (21,'Survey #21',NULL),
+	 (22,'Survey #22',NULL);
+INSERT INTO survey (id,email,ip_address) VALUES
+	 (23,'Survey #23',NULL),
+	 (25,'Survey #25',NULL),
+	 (26,'Survey #26',NULL),
+	 (27,'Survey #27',NULL),
+	 (28,'Survey #28',NULL),
+	 (29,'Survey #29',NULL),
+	 (31,'Survey #31',NULL);
